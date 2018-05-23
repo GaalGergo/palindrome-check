0128449a3244491cd9ad39445eb419e50a37296e
GaalGergo <gergo0122@gmail.com>
2018-05-23 12:44:48 +0200

    Implement to method and task completed!

--- src/main/java/com/github/gaal/gergo/palindromecheck/PalindromeCheck.java
+++ src/main/java/com/github/gaal/gergo/palindromecheck/PalindromeCheck.java
@@ -1,4 +1,20 @@
 package com.github.gaal.gergo.palindromecheck;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PalindromeCheck {
+    public boolean isPalindrome(String word){
+        String reverseWord = new StringBuilder(word).reverse().toString();
+
+        List<String> reverseList = Arrays.asList(reverseWord.split(""));
+        List<String> wordList = Arrays.asList(word.split(""));
+
+        if(reverseList.equals(wordList)){
+            System.out.println(word);
+            System.out.println(reverseWord);
+            return true;
+            }
+            return false;
+    }
 }
--- src/test/java/com/github/gaal/gergo/palindromecheck/PalindromeCheckTest.java
+++ src/test/java/com/github/gaal/gergo/palindromecheck/PalindromeCheckTest.java
@@ -1,4 +1,30 @@
 package com.github.gaal.gergo.palindromecheck;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PalindromeCheckTest {
+
+    private PalindromeCheck palindromeCheck = new PalindromeCheck();
+
+    @Test
+    public void palindromeTrue1(){
+        Assert.assertTrue(palindromeCheck.isPalindrome("abcba"));
+    }
+
+    @Test
+    public void palindromeTrue2(){
+        Assert.assertTrue(palindromeCheck.isPalindrome("ababa"));
+    }
+
+    @Test
+    public void palindromeFalse1(){
+        Assert.assertFalse(palindromeCheck.isPalindrome("abcaa"));
+    }
+
+    @Test
+    public void palindromeFalse2(){
+        Assert.assertFalse(palindromeCheck.isPalindrome("aaaabbbbaa"));
+    }
+
 }
